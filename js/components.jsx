13eefecac5091278d11275d765018c27d507847f
Karan Sekhri <ksekhri@gmail.com>
2017-05-04 17:51:16 -0700

    Add AJAX request to PortfolioBody
    Add AJAX request to PortfolioBody to request JSON projects file

--- js/components.jsx
+++ js/components.jsx
@@ -69,6 +69,40 @@ class AboutBody extends React.Component {
 	}
 }
 class PortfolioBody extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			projects: null
+		};
+	}
+	fetchProjects(){
+		// Create XHR request
+		var request = new XMLHttpRequest();
+
+		// Create request to XHR object
+		// 3 parameters to open: 1) Method: GET/POST 2) Location of data file 3) Boolean asynch? - Default true
+		request.open('GET','../json/projects.json');
+
+		// Send request to server
+		request.send();
+
+		// Check request status and ready state
+		// Browser API maintains readyState property indicating how far request has processed
+		// 0 Request not initialized
+		// 1 Server connection established
+		// 2 Request received
+		// 3 Processing Request
+		// 4 Request finished & response ready
+		request.onreadystatechange = () => {
+			if(request.status===200 && request.readyState === 4) {
+				this.setState({projects: JSON.parse(request.responseText)});
+			}
+		}
+		// OR request.onreadystatechange = functionName;
+	}
+	componentWillMount() {
+		this.fetchProjects();
+	}
 	render() {
 		return (
 			<div className="row">
