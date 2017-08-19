9a83f2a15ec5999f973abd72bbe5626339d8948a
Karan Sekhri <ksekhri@gmail.com>
2017-08-19 15:30:26 -0700

    Inject Routes.jsx as root element of app

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { App } from './components/App.jsx'
+import { Routes } from './Routes.jsx'
 
 window.React = React;
 
 render(
-	<App />, document.getElementById('ks-app-wrapper')
+	<Routes />, document.getElementById('ks-app-wrapper')
 );
